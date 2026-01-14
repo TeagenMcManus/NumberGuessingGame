b7b1dbb548f89628b236126afaa18111d5747688
Teagen.A.M <88509144+TeagenMcManus@users.noreply.github.com>
2026-01-14 20:08:37 +0000

    added the input for users name

--- guessingGame/guessingGame.c
+++ guessingGame/guessingGame.c
@@ -3,29 +3,13 @@
 #include <time.h>
 
 int main() {
-    srand(time(NULL));
-    int secretNumber = rand() % 100 + 1;
-    int guess = 0;
-    int attempts = 0;
     char userName[24];
 
     printf("What is your name?: ");
     scanf("%s", userName);
     printf("Hello, %s! Welcome to the Guessing Game!", userName);
 
-   // while (guess != secretNumber) {
-    //    printf("Enter your guess: ");
-    //    scanf("%d", &guess);
-    //    attempts++;
-//
-      //  if (guess < secretNumber) {
-      //      printf("Too low! Try again.\n");
-     //   } else if (guess > secretNumber) {
-          //  printf("Too high! Try again.\n");
-       // } else {
-     //       printf("Correct! You guessed the number in %d attempts.\n", attempts);
-    //    }
-   // }
+   
 
     return 0;
 }
